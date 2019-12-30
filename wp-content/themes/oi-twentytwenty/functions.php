27a09db21f464eb8d45924ff889507d232055799
sh14 <sh-v@ya.ru>
2019-12-30 20:40:45 +0300

    adminbar styles deleted

--- wp-content/themes/oi-twentytwenty/functions.php
+++ wp-content/themes/oi-twentytwenty/functions.php
@@ -10,6 +10,7 @@ function register_styles(){
 	wp_dequeue_style('wp-block-library');
 //	wp_dequeue_style('crayon');
 	wp_deregister_style('crayon-font-monaco');
+	wp_deregister_style('wbcr-clearfy-adminbar-styles');
 	wp_deregister_script( 'comment-reply' );
 	wp_deregister_script( 'wp-embed' );
 //	wp_enqueue_style('prefers-color-scheme','/assets/styles/style.css',[],'1');
@@ -30,4 +31,32 @@ add_filter( 'excerpt_length', function($length) {
 } );
 
 
+
+
+function fff(){
+	//Do something with the buffer (HTML)
+//	return $buffer;
+//	global $post;
+	$link = get_permalink(get_the_ID());
+	$response = wp_remote_get($link,['timeout' => 120,]);
+	$body = 'no';
+	// проверим правильный ли получили ответ
+	if ( is_wp_error( $response ) ){
+		echo $response->get_error_message();
+	}
+	elseif( wp_remote_retrieve_response_code( $response ) === 200 ){
+		// Все OK, делаем что нибудь с данными $request['body']
+		$body = wp_remote_retrieve_body( $response );
+	}
+	echo $body;
+	die();
+//	$file = file_get_contents($link);
+//	if(!file_exists('/asdgg.html')){
+//		file_put_contents('/asdgg.html',$file);
+//	}
+//	echo 'ok';die;
+}
+//add_action('wp','fff');
+//add_action('wp_print_footer_scripts','fff');
+//add_action('shutdown','fff');
 // eof
